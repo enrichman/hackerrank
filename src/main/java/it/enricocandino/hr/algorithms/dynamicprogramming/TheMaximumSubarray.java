4b1fe062ac63df6ab8b9c372a3425ed671ecb22d
Enrico Candino <enrico.candino@datalytics.it>
2016-07-20 13:36:56 +0200

    teh maximum subarray (start)

---
+++ src/main/java/it/enricocandino/hr/algorithms/dynamicprogramming/TheMaximumSubarray.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C) Datalytics - All Rights Reserved
+ * Created by Enrico Candino <enrico.candino@datalytics.it> on Jul,, 20.
+ */
+package it.enricocandino.hr.algorithms.dynamicprogramming;
+
+import it.enricocandino.hr.Solution;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class TheMaximumSubarray extends Solution {
+
+    public void solve(File file) throws Exception {
+        Scanner in = new Scanner(file);
+        int testCases = in.nextInt();
+        for(int t=0; t<testCases; t++) {
+            
+        }
+    }
+
+}
